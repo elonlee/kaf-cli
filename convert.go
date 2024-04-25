f98d00ae475a81f3c2f06f935fa837fb9a1058a1
elonlee <elonlee@163.com>
2024-04-25 06:13:59 +0000

    修改几个默认值

--- convert.go
+++ convert.go
@@ -6,10 +6,6 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/simplifiedchinese"
-	"golang.org/x/text/transform"
 	_ "image/jpeg"
 	_ "image/png"
 	"io"
@@ -21,6 +17,11 @@ import (
 	"strings"
 	"time"
 	"unicode/utf8"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
 )
 
 type Book struct {
@@ -99,7 +100,7 @@ func NewBookArgs() *Book {
 	flag.StringVar(&book.Match, "match", "", "匹配标题的正则表达式, 不写可以自动识别, 如果没生成章节就参考教程。例: -match 第.{1,8}章 表示第和章字之间可以有1-8个任意文字")
 	flag.StringVar(&book.VolumeMatch, "volume-match", VolumeMatch, "卷匹配规则,设置为false可以禁用卷识别")
 	flag.StringVar(&book.UnknowTitle, "unknow-title", "章节正文", "未知章节默认名称")
-	flag.StringVar(&book.Cover, "cover", "cover.png", "封面图片可为: 本地图片, 和orly。 设置为orly时生成orly风格的封面, 需要连接网络。")
+	flag.StringVar(&book.Cover, "cover", "cover.jpg", "封面图片可为: 本地图片, 和orly。 设置为orly时生成orly风格的封面, 需要连接网络。")
 	flag.StringVar(&book.CoverOrlyColor, "cover-orly-color", "", "orly封面的主题色, 可以为1-16和hex格式的颜色代码, 不填时随机")
 	flag.IntVar(&book.CoverOrlyIdx, "cover-orly-idx", -1, "orly封面的动物, 可以为0-41, 不填时随机, 具体图案可以查看: https://orly.nanmu.me")
 	flag.UintVar(&book.Max, "max", 35, "标题最大字数")
@@ -109,9 +110,9 @@ func NewBookArgs() *Book {
 	flag.StringVar(&book.LineHeight, "line-height", "", "行高(用于设置行间距, 默认为1.5rem)")
 	flag.StringVar(&book.Font, "font", "", "嵌入字体, 之后epub的正文都将使用该字体")
 	flag.StringVar(&book.Lang, "lang", GetEnv("KAF_CLI_LANG", "zh"), "设置语言: en,de,fr,it,es,zh,ja,pt,ru,nl。环境变量KAF_CLI_LANG可修改默认值")
-	flag.StringVar(&book.Format, "format", GetEnv("KAF_CLI_FORMAT", "all"), "书籍格式: all、epub、mobi、azw3。环境变量KAF_CLI_FORMAT可修改默认值")
+	flag.StringVar(&book.Format, "format", GetEnv("KAF_CLI_FORMAT", "epub"), "书籍格式: all、epub、mobi、azw3。环境变量KAF_CLI_FORMAT可修改默认值")
 	flag.StringVar(&book.Out, "out", "", "输出文件名，不需要包含格式后缀")
-	flag.BoolVar(&book.Tips, "tips", true, "添加本软件教程")
+	flag.BoolVar(&book.Tips, "tips", false, "添加本软件教程")
 	flag.Parse()
 	return &book
 }
@@ -123,10 +124,11 @@ func (book *Book) SetDefault() {
 	book.Max = defalutInt(book.Max, 35)
 	book.Indent = defalutInt(book.Indent, 2)
 	book.Align = defaultString(book.Align, GetEnv("KAF_CLI_ALIGN", "center"))
-	book.Cover = defaultString(book.Cover, "cover.png")
+	book.Cover = defaultString(book.Cover, "cover.jpg")
 	book.Bottom = defaultString(book.Bottom, "1em")
 	book.Lang = defaultString(book.Lang, GetEnv("KAF_CLI_LANG", "zh"))
-	book.Format = defaultString(book.Format, GetEnv("KAF_CLI_FORMAT", "all"))
+	book.Format = defaultString(book.Format, GetEnv("KAF_CLI_FORMAT", "epub"))
+	book.Tips = false
 }
 func (book *Book) Check(version string) error {
 	book.version = version
